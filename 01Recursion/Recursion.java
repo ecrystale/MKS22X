97c3a4c8eaffdf797446d09c734d5d9ac392c907
Emily <elee19@stuy.edu>
2018-02-05 21:25:41 -0500

    add sqrt

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,6 +1,6 @@
 public class Recursion{
     //1.1a: factorial
-    public int fact(int n){
+    public static int fact(int n){
 	if (n<0){
 	    throw new IllegalArgumentException();
 	}
@@ -11,7 +11,7 @@ public class Recursion{
     }
     
     //1.1b: Fibonacci
-    public int fib(int n){
+    public static int fib(int n){
 	if (n<0){
 	    throw new IllegalArgumentException();
 	}
@@ -24,11 +24,32 @@ public class Recursion{
 	return fib(n-1)+fib(n-2);
     }
 
-    //1.1c: Square Root
-    public double sqrt(double n){
 
+   //1.1c: Square Root
+    public static double sqrt(double n){
+	if (n<0){
+	    throw new IllegalArgumentException();
+	}
+	return guess(n,1);
     }
 
-public 
+    public static double guess(double n, double g){
+	double y=((n/g)+g)/2;
+	if (Math.abs(g-y)<0.00001){
+	    return y;
+	}
+	return guess(n,y);
+    }
+
+    public static void main(String[] args){
+	System.out.println(fact(0));
+	System.out.println(fact(10));
+	System.out.println(fib(0));
+	System.out.println(fib(1));
+	System.out.println(fib(2));
+	System.out.println(fib(10));
+	System.out.println(sqrt(100));
+	System.out.println(sqrt(2));
+    }
 
 }
