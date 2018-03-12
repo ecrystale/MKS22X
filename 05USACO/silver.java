56d3fd5bdd731b32c875f454ca458ca9986f76fc
Emily <elee19@stuy.edu>
2018-03-11 20:27:15 -0400

    adding silver back to USACO

--- 05USACO/silver.java
+++ 05USACO/silver.java
@@ -1,15 +1,36 @@
 import java.util.*;
 import java.io.*;
-public class USACO{
-    private static int[][] field;
+public class silver{
+    private static int[3] start;
+    private static int[4] end;
+    private static char[][] field;
     public static int silver(String filename)throws FileNotFoundException{
 	File text= new File(filename);
 	Scanner inf= new Scanner(text);
 	int row=0;	    
 	int r=0;
-	String setup=inf.nextLine();
-	setupary(setup);
+	String setups=inf.nextLine();
+	setupsilver(setups);
 	lake.toString();
+    }
+    public static void setupsilver(String starting){
+	int setplace=0;
+	int place=0;
+	for(int i=0;i<setup.length();i++){
+	    if(setup.substring(i,i+1).equals(" ")){
+		set[setplace]=Integer.parseInt(setup.substring(place,i));
+		place=i+1;
+		setplace++;
+	    }
+	    if(i==setup.length()-1){
+		set[setplace]=Integer.parseInt(setup.substring(place,i+1));
+		place=i+1;
+		setplace++;;
+	    }
+	}
+	lake=new int[set[0]][set[1]];
+	elevation=set[2];
+	calls=new int[set[3]][3];
     }
     public static boolean cantravel(int row, int col){
 	if(row>=0 && row<lake.length && col>=0 && col<lake[0].length){
