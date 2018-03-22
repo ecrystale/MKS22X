92a67c93da6b0b34bc2bb083322fbeafee6bfa38
Emily <elee19@stuy.edu>
2018-03-21 23:33:58 -0400

    improved, but still not accurate

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,38 +1,106 @@
 public class Merge{
-    public static void mergesort(int[] temp, int[] data, int lo, int hi){
-	int mid=(lo+hi)/2;
-	if(lo<hi){
-	    mergesort(temp,data,lo,mid);
-	    mergesort(temp,data,mid+1,hi);
+    public static void mergesort(int[] data){
+	int[] temp=new int[data.length];
+	for(int i=0;i<data.length;i++){
+	    temp[i]=data[i];
 	}
-	merge(data,temp,lo,mid,hi);
+	msort(temp,data,0,data.length-1);
+    }
+    public static void msort(int[] temp, int[] data, int lo, int hi){
+	int mid=(lo+hi)/2;
+	String all="";
+	for(int i=lo;i<mid+1;i++){
+	    all+=temp[i]+" ";
+	}
+	System.out.println(all);
+	all="";
+	for(int i=mid+1;i<hi+1;i++){
+	    all+=temp[i]+" ";
+	}
+	System.out.println(all);
+	all="";
+	if(lo<hi){
+	    msort(temp,data,lo,mid);
+	    msort(temp,data,mid+1,hi);
+	    merge(data,temp,lo,mid,hi);
+	}
     }
     public static void merge(int[] data,int[] temp,int lo,int mid, int hi){
 	int orig=mid;
-	for(int i=lo;i<hi;i++){
-	    if(lo==mid){
-		for(int j=mid+1;j<=hi;j++){
-		    data[i]=temp[j];
-		    i++;
-		}
-	    }
-	    if(hi==mid+1){
-		for(int j=lo;j<=mid;j++){
-		    data[i]=temp[j];
-		    i++;
-		}
-	    }
-	    if(temp[lo]>temp[mid+1]){
-		data[i]=temp[mid+1];
-		mid++;
-	    }
-	    if(temp[lo]<temp[mid+1]){
-		data[i]=temp[lo];
-		lo++;
-	    }
+	int origl=lo;	
+	String all="";
+	for(int i=lo;i<mid+1;i++){
+	    all+=temp[i]+" ";
 	}
-	/**
-	   while(hi!=mid+1 && lo!=orig){
+	int i=lo;
+	while(lo<orig+1 &&  mid<=hi){
+	    System.out.println("ok "+(all));
+	    System.out.println(temp[lo]);
+	    if(lo==orig){
+		for(int j=mid;j<hi;j++){
+		    System.out.println("temp "+temp[j]);
+		    data[i]=temp[j];
+		    i++;
+		    mid++;
+		}
+	    }
+	    if(hi==mid){
+		for(int j=lo;j<orig+1;j++){
+		    System.out.println("ok0 "+temp[lo]);
+		    data[i]=temp[j];
+		    lo++;
+		    i++;
+		}
+	    }
+	    else{
+		if(temp[lo]>temp[mid]){
+		    System.out.println(lo);
+		    System.out.println("ok2 "+temp[lo]);
+		    System.out.println("ok2 "+temp[mid]);
+		    data[i]=temp[mid];
+		    System.out.println("ok2 "+temp[lo]);
+		    System.out.println(lo);
+		    System.out.println("ok2 "+temp[mid]);
+		    mid++;
+		    i++;
+		}
+		else{
+		    System.out.println("ok3 "+temp[lo]);
+		    System.out.println("ok34 "+data[i]);
+		    //swap(data,temp,i,lo);
+		    data[i]=temp[lo];
+		    System.out.println("ok34 "+data[i]);
+		    lo++;
+		    i++;
+		}
+	    }
+	    all="";
+	    for(int k=origl;k<hi;k++){
+		all+=temp[k]+" ";
+	    }
+	    System.out.println("Resultstemp "+all);
+	    all="";
+	    for(int k=origl;k<hi;k++){
+		all+=data[k]+" ";
+	    }
+	    System.out.println("Results "+all);
+	}
+	for(int k=origl;k<hi;k++){
+	    temp[k]=data[k];
+	}
+	all="";
+	for(int k=origl;k<hi;k++){
+	    all+=temp[k]+" ";
+	}
+	System.out.println("alltemp "+all);
+	all="";
+	for(int k=origl;k<hi;k++){
+	    all+=data[k]+" ";
+	}
+	System.out.println("all "+all);
+    }
+    /**
+       while(hi!=mid+1 && lo!=orig){
 	   if(temp[lo]>temp[mid+1]){
 	   data[lo]=mid+1;
 	   mid++;
@@ -48,13 +116,12 @@ public class Merge{
 	   }
 	   hi++;
 	   }*/
-    }
-    public static int[] swap(int[] data,int lo, int hi){
-	int i=data[lo];
-	int j=data[hi];
-	data[lo]=j;
-	data[hi]=i;
-	return data;
+    
+    public static void swap(int[] data,int[] temp, int one, int two){
+	int i=data[one];
+	int j=temp[two];
+	data[one]=j;
+	//return data;
     }
     public static void toString(int[] data){
 	String all="";
@@ -64,9 +131,10 @@ public class Merge{
 	System.out.println(all);
     }
     public static void main(String[] args){
-	int[] ary = { 2, 10, 15, 23, 0,  5};
+	int[] ary= { 2, 10, 15, 23, 0,  5};
+	int[] ary2= { 2, 10, 15, 23, 0,  5};
 	toString(ary);
-	mergesort(ary,ary,0,ary.length-1);
+	mergesort(ary);
 	toString(ary);
     }
 }
