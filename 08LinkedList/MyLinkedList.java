d1538e55837536d2455f24b8d90ec928dc33c829
Emily <elee19@stuy.edu>
2018-03-28 00:07:38 -0400

    starting

---
+++ 08LinkedList/MyLinkedList.java
@@ -0,0 +1,28 @@
+java.util.LinkedList<E>;
+public class MyLinkedList{
+    Node first;
+    Node last;
+    int length;
+    public MyLinkedList(){
+	new LinkedList();
+    }
+    public int remove(int value){
+	length--;
+    }
+    public void add(index,value){
+	length++;
+    }   
+    public String toString(){
+	String all="[";
+	for(int i=0;
+    }
+    public int get(int index){
+	return ;
+    }
+    public void set(int index,int value){
+
+    }
+    public int size(){
+	return length;
+    }
+}
---
+++ 08LinkedList/Node.java
@@ -0,0 +1,25 @@
+private class Node{
+    Node next,prev;
+    int data;
+    private Node getNext(){
+
+    }
+    private Node getPrev(){
+
+    }
+    private int getValue(){
+
+    }
+    private Node setNext(){
+
+    }
+    private Node setPrev(){
+
+    }
+    private int setValue(){
+
+    }
+    private String toString(){
+
+    }
+}
