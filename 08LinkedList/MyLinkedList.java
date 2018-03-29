95b015eb1a014613e22c0254c1c395098e4366aa
ecrystale <elee19@stuy.edu>
2018-03-29 15:09:59 -0400

    working on linkedlist

--- 08LinkedList/MyLinkedList.java
+++ 08LinkedList/MyLinkedList.java
@@ -1,28 +1,57 @@
-Njava.util.LinkedList<E>;
 public class MyLinkedList{
-    Node first;
-    Node last;
-    int length;
-    public MyLinkedList(){
-	new LinkedList();
-    }
-    public int remove(int value){
-	length--;
-    }
-    public void add(index,value){
-	length++;
-    }   
-    public String toString(){
-	String all="[";
-	for(int i=0;
-    }
-    public int get(int index){
-	return ;
-    }
-    public void set(int index,int value){
+  private Node start,end;
+  private int size;
 
+  //This method will help you write other
+  //methods, it is private to protect your list
+    private Node getNode(int index){
+	return index.getNode();
+    }
+
+
+    public MyLinkedList(){
+	size=0;
+	start=0;
+	end=0;
+    }
+    public String toString(){
+	//for(
+    }
+    public void clear(){
+	size=0;
+	start=0;
+	end=0;
     }
     public int size(){
-	return length;
+	return size;
     }
+    public Integer get(int index){//exceptions!
+
+    }
+    public Integer set(int index, Integer value){//exceptions!
+
+    }
+
+    public int indexOf(Integer value){
+
+    }
+
+    public boolean add(Integer newData){
+
+    }
+    public void add(int index, Integer value){//exceptions!
+
+    }
+
+  //The remove methods can cause a problem, this is why we shouldn't 
+  //use an int as the data, we need objects to distinguish between index and data
+    public boolean remove(Integer value){
+
+    }
+    public boolean remove(int index){//exceptions!
+	if(0<=index && index<size){
+	    index-1.setNext(null);
+	}
+    }
+
 }
--- 08LinkedList/Node.java
+++ 08LinkedList/Node.java
@@ -2,24 +2,24 @@ private class Node{
     Node next,prev;
     int data;
     private Node getNext(){
-
+	return next;
     }
     private Node getPrev(){
-
+	return prev;
     }
     private int getValue(){
-
+	return data;
     }
-    private Node setNext(){
-
+    private Node setNext(int i){
+	next=i;
     }
-    private Node setPrev(){
-
+    private Node setPrev(int i){
+	prev=i;
     }
-    private int setValue(){
-
+    private int setValue(int i){
+	data=i;
     }
     private String toString(){
-
+	return data+"";
     }
 }
