dcdcbd19138898a8202dbb4f578216c73aaf7f51
Emily <elee19@stuy.edu>
2018-04-18 06:01:15 -0400

    convert

--- 09Stack/Stack.java
+++ 09Stack/Stack.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Stack{
-    static ArrayList ary=new ArrayList();
-    static Stack hold=new Stack();
+    private static ArrayList ary=new ArrayList();
+    // static Stack hold=new Stack();
     public static void eval(String data){
 	int index=0;
 	for(int i=0;i<data.length();i++){
@@ -14,9 +14,28 @@ public class Stack{
     }
 
     public static void solve(){
-	for(int i=0;i<ary.length();i++){
-	    hold.push(ary[i]);
+	String toSolve="";
+	double result;
+	for(int i=0;i<ary.size();i++){
+	    if((ary.get(i)).equals("+") || (ary.get(i)).equals("-") || (ary.get(i)).equals("/") || (ary.get(i)).equals("*")){
+		String first=toSolve.substring(0,toSolve.length()-1);
+		String last=toSolve.substring(toSolve.length()-1,toSolve.length());
+		toSolve="("+first+(ary.get(i))+last+")";
+	    }
+	    else{
+		toSolve+=(ary.get(i));
+	    }
+	    // hold.push(ary[i]);
 	}
+/**
+	for(int i=0;i<toSolve.length();i++){
+	    if((ary.get(i)).equals("+") || (ary.get(i)).equals("-") || (ary.get(i)).equals("/") || (ary.get(i)).equals("*")){
+		result=
+		}*/
+	/**while(hold.empty!=true){
+	    if(peek()=="+"){
 
+	    }
+	    }*/
     }
 }
