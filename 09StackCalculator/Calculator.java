232c9fd91eb4a66915023084e94c79b9a1d419da
Emily <elee19@stuy.edu>
2018-04-18 21:06:04 -0400

    closer

--- 09StackCalculator/Calculator.java
+++ 09StackCalculator/Calculator.java
@@ -1,8 +1,11 @@
 import java.util.*;
-public class Calculator{
-    private static ArrayList ary=new ArrayList();
+@SuppressWarnings("unchecked")
+public class Calculator{    
+    private static ArrayList<String> ary=new ArrayList();
+    private static ArrayList<Double> nums=new ArrayList();
     // static Stack hold=new Stack();
     public static void eval(String data){
+        //ary=new ArrayList();
 	int index=0;
 	for(int i=0;i<data.length();i++){
 	    if((data.substring(i,i+1)).equals(" ")){
@@ -10,35 +13,51 @@ public class Calculator{
 		index=i+1;
 	    }
 	}
-	solve();
-    }
-
-    public static void solve(){
 	String toSolve="";
 	double result=0;
 	for(int i=0;i<ary.size();i++){
+	    System.out.println(ary.get(i));
+	    if((ary.get(i)).equals("+")){
+		result=nums.get(0)+nums.get(1);
+		nums.set(0,result);
+		nums.remove(1);
+		//nums.remove(i-1);
+	    }
+	    if((ary.get(i)).equals("-")){
+	        result=nums.get(0)-nums.get(1);
+		nums.set(0,result);
+		nums.remove(1);
+	    }
+	    if((ary.get(i)).equals("/")){
+	        result=nums.get(0)/nums.get(1);
+		nums.set(0,result);
+		nums.remove(1);
+	    }
+	    if((ary.get(i)).equals("*")){
+	        result=nums.get(0)*nums.get(1);
+		nums.set(0,result);
+		nums.remove(1);
+	    }
+	    else{
+		nums.add(i,Double.parseDouble(ary.get(i)));
+	    }
+	    /**
 	    if((ary.get(i)).equals("+") || (ary.get(i)).equals("-") || (ary.get(i)).equals("/") || (ary.get(i)).equals("*")){
 		String first=toSolve.substring(0,toSolve.length()-1);
 		String last=toSolve.substring(toSolve.length()-1,toSolve.length());
 		toSolve="("+first+(ary.get(i))+last+")";
-	    }
+		}
 	    else{
 		toSolve+=(ary.get(i));
-	    }
-	    // hold.push(ary[i]);
-	}
-	System.out.println(toSolve);
-/**
-	for(int i=0;i<toSolve.length();i++){
-	    if((ary.get(i)).equals("+") || (ary.get(i)).equals("-") || (ary.get(i)).equals("/") || (ary.get(i)).equals("*")){
-		result=
 		}*/
-	/**while(hold.empty!=true){
-	    if(peek()=="+"){
 
-	    }
-	    }*/
+	}
+	    //System.out.println(toSolve);
+	    System.out.println(result);
+
     }
+
+
 
     public static void main(String[] args){
 	eval("10 2.0 +");// is 12.0
