34ce10d2b525116030c30a4bf890ff8f9317d2b1
Emily <elee19@stuy.edu>
2018-04-28 17:13:16 -0400

    full heap, not tested

---
+++ 12MyHeap/MyHeap.java
@@ -0,0 +1,106 @@
+public class MyHeap{
+    boolean max;
+    String[] data;
+    int size=0;
+    public MyHeap(){// - construct empty max heap
+	max=true;
+	data= new String[10];
+    }
+    public MyHeap(boolean rule){// - true: construct empty max heap, false: construct empty min heap.
+	max=rule;
+    }
+	// Methods
+    public void add(String s){
+	if(max==true){
+	    maxadd(s);
+	}
+	if(max==false){
+	    minadd(s);
+	}
+    }
+    public void maxadd(String s){
+	if(size==data.length){
+	    resize();
+	}
+	data[size]=s;
+	pull(size);
+	size++;
+    }
+    public void minadd(String s){
+	if(size==data.length){
+	    resize();
+	}
+	data[size]=s;
+	pull(size);
+	size++;
+    }
+    public void pull(int index){
+	int place=(index-1)/2;
+	if(place>=0){
+	    if(max==true){
+		if(data[place].compareTo(data[index])<0){
+		    swap(place,index);
+		    pull(place);
+		}
+	    }
+	    if(max==false){
+		if(data[place].compareTo(data[index])<0){
+		    swap(place,index);
+		    pull(place);
+		}
+	    }
+	}
+    }
+    public void swap(int one, int two){
+	String orig=data[one];
+	data[one]=data[two];
+	data[two]=orig;
+    }
+    public void resize(){
+	int index=data.length*2;
+	String[] copy=new String[index];
+	for(int i=0;i<size;i++){
+	    copy[i]=data[i];
+	}
+	data=copy;
+    }
+    public String remove(){
+	String original=data[0];
+	data[0]=data[size-1];
+	data[size-1]=null;
+	size--;
+	push(0);
+	return original;
+    }
+    public void push(int index){
+	int place=2*index+1;
+	if(place<=size){
+	    if(max==true){
+		if(data[place].compareTo(data[index])>0){
+		    swap(place,index);
+		    push(place);
+		}
+		if(data[place+1].compareTo(data[index])>0){
+		    swap(place+1,index);
+		    push(place+1);
+		}
+	    }
+	    if(max==false){
+		if(data[place].compareTo(data[index])<0){
+		    swap(place,index);
+		    push(place);
+		}
+		if(data[place+1].compareTo(data[index])<0){
+		    swap(place+1,index);
+		    push(place+1);
+		}
+	    }
+	}
+    }
+    public String peek(){
+	return data[0];
+    }
+    public int size(){
+	return size;
+    }
+}
