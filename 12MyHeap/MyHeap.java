96bb031d1fbb18fd0398f2431fcb86a66c50829e
ecrystale <elee19@stuy.edu>
2018-04-30 15:35:31 -0400

    testing

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -1,19 +1,19 @@
 public class MyHeap<T extends Comparable<T>>{
-    boolean max;
-    T[] data;
-    int size=0;
+    private static boolean max;
+    private static String[] data;
+    private static int size=0;
     @SuppressWarnings("unchecked")
     public MyHeap(){// - construct empty max heap
 	max=true;
-        data = (T[])new Comparable[10];
+        data = (String[])new Comparable[10];
     }
     @SuppressWarnings("unchecked")
     public MyHeap(boolean rule){// - true: construct empty max heap, false: construct empty min heap.
 	max=rule;
-	data = (T[])new Comparable[10];
+	data = (String[])new Comparable[10];
     }
 	// Methods
-    public void add(T s){
+    public static void add(String s){
 	if(max==true){
 	    maxadd(s);
 	}
@@ -21,7 +21,7 @@ public class MyHeap<T extends Comparable<T>>{
 	    minadd(s);
 	}
     }
-    public void maxadd(T s){
+    public static void maxadd(String s){
 	if(size==data.length){
 	    resize();
 	}
@@ -29,7 +29,7 @@ public class MyHeap<T extends Comparable<T>>{
 	pull(size);
 	size++;
     }
-    public void minadd(T s){
+    public static void minadd(String s){
 	if(size==data.length){
 	    resize();
 	}
@@ -37,7 +37,7 @@ public class MyHeap<T extends Comparable<T>>{
 	pull(size);
 	size++;
     }
-    public void pull(int index){
+    public static void pull(int index){
 	int place=(index-1)/2;
 	if(place>=0){
 	    if(max==true){
@@ -54,29 +54,29 @@ public class MyHeap<T extends Comparable<T>>{
 	    }
 	}
     }
-    public void swap(int one, int two){
-	T orig=data[one];
+    public static void swap(int one, int two){
+	String orig=data[one];
 	data[one]=data[two];
 	data[two]=orig;
     }
     @SuppressWarnings("unchecked")
-    public void resize(){
+    public static void resize(){
 	int index=data.length*2;
-	T[] copy=(T[])new Comparable[index];
+	String[] copy=(String[])new Comparable[index];
 	for(int i=0;i<size;i++){
 	    copy[i]=data[i];
 	}
 	data=copy;
     }
-    public T remove(){
-	T original=data[0];
+    public static String remove(){
+	String original=data[0];
 	data[0]=data[size-1];
 	data[size-1]=null;
 	size--;
 	push(0);
 	return original;
     }
-    public void push(int index){
+    public static void push(int index){
 	int place=2*index+1;
 	if(place<=size){
 	    if(max==true){
@@ -101,10 +101,16 @@ public class MyHeap<T extends Comparable<T>>{
 	    }
 	}
     }
-    public T peek(){
+    public static String peek(){
 	return data[0];
     }
-    public int size(){
+    public static int size(){
 	return size;
+    }
+
+    public static void main(String[] args){
+	MyHeap x=new MyHeap();
+	x.add("x");
+	System.out.println(x);
     }
 }
